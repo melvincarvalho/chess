5b546d886b894a9b3b3dca434d66bbe8a50c8f2d
Melvin Carvalho <melvincarvalho@gmail.com>
2015-07-06 14:26:53 +0200

    add realtime updates

--- app/app.js
+++ app/app.js
@@ -1,8 +1,8 @@
 var board,
-  game = new Chess(),
-  statusEl = $('#status'),
-  fenEl = $('#fen'),
-  pgnEl = $('#pgn');
+game = new Chess(),
+statusEl = $('#status'),
+fenEl = $('#fen'),
+pgnEl = $('#pgn');
 
 
 var f,g;
@@ -45,86 +45,28 @@ App.config(function($locationProvider) {
 
 App.controller('Main', function($scope, $http, $location, $timeout, $sce, LxNotificationService, LxProgressService, LxDialogService) {
 
+
+  // INIT functions
+  //
   // save app configuration if it's the first time the app runs
   $scope.initApp = function() {
     $scope.init();
   };
 
-  $scope.TLSlogin = function() {
-    $scope.loginTLSButtonText = 'Logging in...';
-    $http({
-      method: 'HEAD',
-      url: AUTHENDPOINT,
-      withCredentials: true
-    }).success(function(data, status, headers) {
-      // add dir to local list
-      var user = headers('User');
-      if (user && user.length > 0 && user.slice(0,4) == 'http') {
-        LxNotificationService.success('Login Successful!');
-        $scope.loggedIn = true;
-        $scope.user = user;
-      } else {
-        LxNotificationService.error('WebID-TLS authentication failed.');
-        console.log('WebID-TLS authentication failed.');
-      }
-      $scope.loginTLSButtonText = 'Login';
-    }).error(function(data, status, headers) {
-      LxNotificationService.error('Could not connect to auth server: HTTP '+status);
-      console.log('Could not connect to auth server: HTTP '+status);
-      $scope.loginTLSButtonText = 'Login';
-    });
-  };
+  // set init variables
+  $scope.init = function() {
 
-
-  $scope.setBoard = function (position) {
-    $scope.position = position;
-    board.position(position, false);
-    game.load(position);
-  };
-
-  $scope.openDialog = function(elem, reset) {
-    if (reset) {
-      $scope.resetContact();
-    }
-    LxDialogService.open(elem);
-    $(document).keyup(function(e) {
-      if (e.keyCode===27) {
-        LxDialogService.close(elem);
-      }
-    });
-  };
-
-  $scope.save = function() {
-    var position = $scope.position;
-    if (!position) {
-      LxNotificationService.error('position is empty');
-      return;
-    }
-    console.log(position);
-
-    $http({
-      method: 'PUT',
-      url: $scope.storageURI,
-      withCredentials: true,
-      headers: {
-        "Content-Type": "text/turtle"
-      },
-      data: '<#this> '+ URN('fen') +' """' + position + '""" .',
-    }).
-    success(function(data, status, headers) {
-      LxNotificationService.success('Position saved');
-      $location.search('storageURI', $scope.storageURI);
-      $scope.setBoard(position);
-    }).
-    error(function(data, status, headers) {
-      LxNotificationService.error('could not save position');
-    });
+    $scope.initStore();
+    $scope.initBoard();
+    $scope.fetchBoard();
+    $scope.initUI();
 
   };
 
-  $scope.logout = function() {
-    $scope.init();
-    LxNotificationService.success('Logout Successful!');
+  $scope.initUI = function() {
+    $scope.initialized = true;
+    $scope.loggedIn = false;
+    $scope.loginTLSButtonText = "Login";
   };
 
   $scope.initStore = function() {
@@ -141,13 +83,12 @@ App.controller('Main', function($scope, $http, $location, $timeout, $sce, LxNoti
 
   $scope.initBoard = function() {
 
-
     // do not pick up pieces if the game is over
     // only pick up pieces for the side to move
     var onDragStart = function(source, piece, position, orientation) {
       if (game.game_over() === true ||
-          (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
-          (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+      (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+      (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
         return false;
       }
     };
@@ -202,9 +143,10 @@ App.controller('Main', function($scope, $http, $location, $timeout, $sce, LxNoti
         }
       }
 
-      statusEl.html(status);
-      fenEl.html(game.fen());
-      pgnEl.html(game.pgn());
+
+      $scope.status = status;
+      $scope.fen = game.fen();
+      $scope.pgn = game.pgn();
     };
 
     var cfg = {
@@ -217,45 +159,195 @@ App.controller('Main', function($scope, $http, $location, $timeout, $sce, LxNoti
     board = new ChessBoard('board', cfg);
 
     updateStatus();
-
-
   };
 
 
 
-  // set init variables
-  $scope.init = function() {
+  // RENDER functions
+  //
+  //
+  $scope.render = function() {
+    $scope.renderBoard();
+  };
 
-    $scope.initStore();
-    $scope.initBoard();
+  $scope.renderBoard = function () {
+    if (!$scope.position) return;
+
+    board.position($scope.position, false);
+    game.load($scope.position);
+    $scope.fen = game.fen();
+  };
+
+  $scope.refresh = function() {
+    $scope.fetchBoard();
+    $scope.render();
+  };
 
 
+  // QUEUE functions
+  //
+  //
 
-    //var c = new ChessBoard('chess', { "position": 'start', 'draggable' : true});
-
-    $scope.initialized = true;
-    $scope.loggedIn = false;
-    $scope.loginTLSButtonText = "Login";
-    // display elements object
-
+  // FETCH functions
+  //
+  //
+  $scope.fetchBoard = function (position) {
     var storageURI = 'https://chess.databox.me/Public/.chess/test';
     if ($location.search().storageURI) {
       storageURI = $location.search().storageURI;
     }
     $scope.storageURI = storageURI;
+    connectToSocket($scope.storageURI);
 
-    f.nowOrWhenFetched(storageURI, undefined, function(ok, body) {
+
+    f.requestURI(storageURI, undefined, true, function(ok, body) {
       var p = g.statementsMatching(undefined, URN('fen'));
       if (p.length) {
-        var position = p[0].object.value;
-        $scope.setBoard(position);
+        $scope.position = p[0].object.value;
+        $scope.render();
       }
+    });
+  };
+
+  $scope.invalidate = function(uri) {
+    console.log('invalidate : ' + uri);
+    f.unload(uri);
+    f.refresh($rdf.sym(uri));
+  };
+
+  // HELPER functions
+  //
+  //
+  $scope.openDialog = function(elem, reset) {
+    if (reset) {
+      $scope.resetContact();
+    }
+    LxDialogService.open(elem);
+    $(document).keyup(function(e) {
+      if (e.keyCode===27) {
+        LxDialogService.close(elem);
+      }
+    });
+  };
+
+  $scope.save = function() {
+    var position = $scope.position;
+    if (!position) {
+      LxNotificationService.error('position is empty');
+      return;
+    }
+    console.log(position);
+
+    $http({
+      method: 'PUT',
+      url: $scope.storageURI,
+      withCredentials: true,
+      headers: {
+        "Content-Type": "text/turtle"
+      },
+      data: '<#this> '+ URN('fen') +' """' + position + '""" .',
+    }).
+    success(function(data, status, headers) {
+      LxNotificationService.success('Position saved');
+      $location.search('storageURI', $scope.storageURI);
+      $scope.renderBoard(position);
+    }).
+    error(function(data, status, headers) {
+      LxNotificationService.error('could not save position');
     });
 
   };
 
+  $scope.TLSlogin = function() {
+    $scope.loginTLSButtonText = 'Logging in...';
+    $http({
+      method: 'HEAD',
+      url: AUTHENDPOINT,
+      withCredentials: true
+    }).success(function(data, status, headers) {
+      // add dir to local list
+      var user = headers('User');
+      if (user && user.length > 0 && user.slice(0,4) == 'http') {
+        LxNotificationService.success('Login Successful!');
+        $scope.loggedIn = true;
+        $scope.user = user;
+      } else {
+        LxNotificationService.error('WebID-TLS authentication failed.');
+        console.log('WebID-TLS authentication failed.');
+      }
+      $scope.loginTLSButtonText = 'Login';
+    }).error(function(data, status, headers) {
+      LxNotificationService.error('Could not connect to auth server: HTTP '+status);
+      console.log('Could not connect to auth server: HTTP '+status);
+      $scope.loginTLSButtonText = 'Login';
+    });
+  };
+
+
+
+  $scope.logout = function() {
+    $scope.init();
+    LxNotificationService.success('Logout Successful!');
+  };
+
+  // SOCKETS
+  //
+  //
+  function getWss(uri) {
+    return 'wss://' + uri.split('/')[2];
+  }
+
+  function sendSub(message, socket) {
+    socket.send(message);
+  }
+
+  function connectToSocket(sub) {
+    if ($scope.socket) return;
+
+    var socket;
+
+    var wss = getWss(sub);
+    console.log('connecting to : ' + wss);
+
+    socket = new WebSocket(wss);
+
+    socket.onopen = function(){
+      console.log(sub);
+      $scope.socket = socket;
+    };
+
+    socket.onmessage = function(msg) {
+      console.log('Incoming message : ');
+      var a = msg.data.split(' ');
+      console.log(a[1]);
+
+      $scope.invalidate(a[1]);
+      $scope.fetchBoard();
+
+      Notification.requestPermission(function (permission) {
+        // If the user is okay, let's create a notification
+        if (permission === "granted") {
+          notify = true;
+        }
+      });
+
+    };
+
+    // delay in case socket is still opening
+    var DELAY = 1000;
+    setTimeout(function(){
+      sendSub('sub ' + sub, socket);
+    }, DELAY);
+
+
+  }
 
 
   $scope.initApp();
 
+});
+
+// escape uris
+App.filter('escape', function() {
+  return window.encodeURIComponent;
 });
