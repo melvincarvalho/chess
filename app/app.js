fd392d52913cc36e3f0d213a9009ec64063de668
Melvin Carvalho <melvincarvalho@gmail.com>
2015-11-17 14:38:03 +0100

    Cleanup

--- app/app.js
+++ app/app.js
@@ -5,38 +5,18 @@ fenEl = $('#fen'),
 pgnEl = $('#pgn');
 
 
-var f,g;
-var db;
 
-var CHAT  = $rdf.Namespace("https://ns.rww.io/chat#");
-var CURR  = $rdf.Namespace("https://w3id.org/cc#");
-var DCT   = $rdf.Namespace("http://purl.org/dc/terms/");
-var FACE  = $rdf.Namespace("https://graph.facebook.com/schema/~/");
-var FOAF  = $rdf.Namespace("http://xmlns.com/foaf/0.1/");
-var LIKE  = $rdf.Namespace("http://ontologi.es/like#");
-var LDP   = $rdf.Namespace("http://www.w3.org/ns/ldp#");
-var MBLOG = $rdf.Namespace("http://www.w3.org/ns/mblog#");
-var OWL   = $rdf.Namespace("http://www.w3.org/2002/07/owl#");
-var PIM   = $rdf.Namespace("http://www.w3.org/ns/pim/space#");
-var RDF   = $rdf.Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#");
-var RDFS  = $rdf.Namespace("http://www.w3.org/2000/01/rdf-schema#");
-var SIOC  = $rdf.Namespace("http://rdfs.org/sioc/ns#");
-var SOLID = $rdf.Namespace("http://www.w3.org/ns/solid/app#");
-var URN   = $rdf.Namespace("urn:");
+// for debug only
+var __kb;
+var __scope;
 
-var AUTHENDPOINT = "https://databox.me/";
-var PROXY = "https://rww.io/proxy.php?uri={uri}";
-var TIMEOUT = 5000;
-var DEBUG = true;
 
-var scope = {};
-var gg;
-
-$rdf.Fetcher.crossSiteProxyTemplate=PROXY;
-
-var App = angular.module('myApp', [
-  'lumx',
+/**
+* The main app
+*/
+var App = angular.module('Chess', [
   'ngAudio',
+  'lumx'
 ]);
 
 App.config(function($locationProvider) {
@@ -44,31 +24,53 @@ App.config(function($locationProvider) {
   .html5Mode({ enabled: true, requireBase: false });
 });
 
+App.controller('Main', function($scope, $http, $location, $timeout, ngAudio, LxNotificationService, LxProgressService, LxDialogService) {
+  // Namespaces
+  var CHAT  = $rdf.Namespace("https://ns.rww.io/chat#");
+  var CURR  = $rdf.Namespace("https://w3id.org/cc#");
+  var DCT   = $rdf.Namespace("http://purl.org/dc/terms/");
+  var FACE  = $rdf.Namespace("https://graph.facebook.com/schema/~/");
+  var FOAF  = $rdf.Namespace("http://xmlns.com/foaf/0.1/");
+  var LIKE  = $rdf.Namespace("http://ontologi.es/like#");
+  var LDP   = $rdf.Namespace("http://www.w3.org/ns/ldp#");
+  var MBLOG = $rdf.Namespace("http://www.w3.org/ns/mblog#");
+  var OWL   = $rdf.Namespace("http://www.w3.org/2002/07/owl#");
+  var PIM   = $rdf.Namespace("http://www.w3.org/ns/pim/space#");
+  var RDF   = $rdf.Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+  var RDFS  = $rdf.Namespace("http://www.w3.org/2000/01/rdf-schema#");
+  var SIOC  = $rdf.Namespace("http://rdfs.org/sioc/ns#");
+  var SOLID = $rdf.Namespace("http://www.w3.org/ns/solid/app#");
+  var TMP   = $rdf.Namespace("urn:tmp:");
 
-/**
- * The app controller
- */
-App.controller('Main', function($scope, $http, $location, $timeout, $sce, ngAudio, LxNotificationService, LxProgressService, LxDialogService) {
+  var f,g;
 
-
+  // INIT
   /**
-   * Initialize app
-   */
+  * Init app
+  */
   $scope.initApp = function() {
     $scope.init();
   };
 
   /**
-   * Initialize
-   */
+  * Set Initial variables
+  */
   $scope.init = function() {
 
     $scope.initStore();
     $scope.initBoard();
-    $scope.fetchBoard();
     $scope.initUI();
 
+    /*
+    $scope.initRDF();
+    $scope.initQueryString();
+    $scope.initLocalStorage();
+    */
+
+    __kb = g;
+    __scope = $scope;
   };
+
 
   /**
    * Init UI
@@ -180,11 +182,242 @@ App.controller('Main', function($scope, $http, $location, $timeout, $sce, ngAudi
   };
 
 
-
-  // RENDER functions
   /**
-   * Render the main screen
+  * Get values from localStorage
+  */
+  $scope.initLocalStorage = function() {
+    if (localStorage.getItem('again')) {
+      $scope.again = JSON.parse(localStorage.getItem('again'));
+    } else {
+      $scope.again = [];
+    }
+    if (localStorage.getItem('good')) {
+      $scope.good = JSON.parse(localStorage.getItem('good'));
+    } else {
+      $scope.good = [];
+    }
+    if (localStorage.getItem('easy')) {
+      $scope.easy = JSON.parse(localStorage.getItem('easy'));
+    } else {
+      $scope.easy = [];
+    }
+    if (localStorage.getItem('user')) {
+      var user = JSON.parse(localStorage.getItem('user'));
+      $scope.loginSuccess(user);
+    }
+  };
+
+  /**
+  * init RDF knowledge base
+  */
+  $scope.initRDF = function() {
+    var PROXY = "https://rww.io/proxy.php?uri={uri}";
+    var AUTH_PROXY = "https://rww.io/auth-proxy?uri=";
+    var TIMEOUT = 90000;
+    $rdf.Fetcher.crossSiteProxyTemplate=PROXY;
+
+    g = $rdf.graph();
+    f = $rdf.fetcher(g, TIMEOUT);
+  };
+
+  /**
+  * init from query string
+  */
+  $scope.initQueryString = function() {
+    if ($location.search().max) {
+      $scope.max = $location.search().max;
+    }
+    $scope.setMax($scope.max);
+
+    $scope.storageURI = 'https://melvincarvalho.github.io/data/vocab/czech.ttl';
+    if ($location.search().storageURI) {
+      $scope.storageURI = $location.search().storageURI;
+    }
+    $scope.setStorageURI($scope.storageURI);
+
+    if ($location.search().lang1) {
+      $scope.lang1 = $location.search().lang1;
+    }
+
+    if ($location.search().lang2) {
+      $scope.lang2 = $location.search().lang2;
+    }
+
+  };
+
+  /**
+  * setMax set maximum number of words
+  * @param  {Number} max number or words
+  */
+  $scope.setMax = function(max) {
+    $scope.max = max;
+    $location.search('max', $scope.max);
+  };
+
+  /**
+  * setStorageURI set the storage URI for words
+  * @param  {String} the storage URI for words
+  */
+  $scope.setStorageURI = function(storageURI) {
+    $scope.storageURI = storageURI;
+    $location.search('storageURI', $scope.storageURI);
+  };
+
+
+  // AUTH
+  /**
+  * TLS Login with WebID
+  */
+  $scope.TLSlogin = function() {
+    var AUTHENDPOINT = "https://databox.me/";
+    $scope.loginTLSButtonText = 'Logging in...';
+    $http({
+      method: 'HEAD',
+      url: AUTHENDPOINT,
+      withCredentials: true
+    }).success(function(data, status, headers) {
+      var header = 'User';
+      var scheme = 'http';
+      var user = headers(header);
+      if (user && user.length > 0 && user.slice(0,scheme.length) === scheme) {
+        $scope.loginSuccess(user);
+      } else {
+        $scope.notify('WebID-TLS authentication failed.', 'error');
+      }
+      $scope.loginTLSButtonText = 'Login';
+    }).error(function(data, status, headers) {
+      $scope.notify('Could not connect to auth server: HTTP '+status);
+      $scope.loginTLSButtonText = 'Login';
+    });
+  };
+
+  /**
+  * loginSuccess called after successful login
+  * @param  {String} user the logged in user
+  */
+  $scope.loginSuccess = function(user) {
+    $scope.notify('Login Successful!');
+    $scope.loggedIn = true;
+    $scope.user = user;
+    $scope.fetchAll();
+    localStorage.setItem('user', JSON.stringify(user));
+  };
+
+  /**
+  * Logout
+  */
+  $scope.logout = function() {
+    $scope.init();
+    $scope.notify('Logout Successful!');
+    localStorage.removeItem('user');
+  };
+
+  // FETCH functions
+  //
+  //
+  $scope.fetchAll = function() {
+    $scope.fetchBoard();
+  };
+
+  /**
+   * Fetch the board
+   * @param  {String} position The URI for the position
    */
+  $scope.fetchBoard = function (position) {
+    var storageURI = 'https://chess.databox.me/Public/.chess/test';
+    if ($location.search().storageURI) {
+      storageURI = $location.search().storageURI;
+    }
+    $scope.storageURI = storageURI;
+    connectToSocket($scope.storageURI);
+
+
+    f.requestURI(storageURI, undefined, true, function(ok, body) {
+      var p = g.statementsMatching(undefined, TMP('fen'));
+      if (p.length) {
+        $scope.position = p[0].object.value;
+        $scope.render();
+      }
+    });
+  };
+
+  /**
+   * Invalidate a cached URI
+   * @param  {String} uri The URI to invalidate
+   */
+  $scope.invalidate = function(uri) {
+    console.log('invalidate : ' + uri);
+    f.unload(uri);
+    f.refresh($rdf.sym(uri));
+  };
+
+  /**
+  * fetchSeeAlso fetches the see also
+  */
+  $scope.fetchSeeAlso = function() {
+    var seeAlso = 'https://melvincarvalho.github.io/vocab/data/seeAlso.ttl';
+    if ($location.search().seeAlso) {
+      seeAlso = $location.search().seeAlso;
+    }
+    f.nowOrWhenFetched(seeAlso, undefined, function(ok, body) {
+      console.log('seeAlso fetched from : ' + seeAlso);
+    });
+
+  };
+
+  /**
+   * Save the position
+   */
+  $scope.save = function() {
+    var position = $scope.position;
+    if (!position) {
+      LxNotificationService.error('position is empty');
+      return;
+    }
+    console.log(position);
+
+    $http({
+      method: 'PUT',
+      url: $scope.storageURI,
+      withCredentials: true,
+      headers: {
+        "Content-Type": "text/turtle"
+      },
+      data: '<#this> '+ TMP('fen') +' """' + position + '""" .',
+    }).
+    success(function(data, status, headers) {
+      LxNotificationService.success('Position saved');
+      $location.search('storageURI', $scope.storageURI);
+      $scope.renderBoard(position);
+    }).
+    error(function(data, status, headers) {
+      LxNotificationService.error('could not save position');
+    });
+
+  };
+
+
+  // HELPER
+  /**
+  * Notify
+  * @param  {String} message the message to display
+  * @param  {String} type the type of notification, error or success
+  */
+  $scope.notify = function(message, type) {
+    console.log(message);
+    if (type === 'error') {
+      LxNotificationService.error(message);
+    } else {
+      LxNotificationService.success(message);
+    }
+  };
+
+
+
+  // RENDER
+  /**
+  * render screen
+  */
   $scope.render = function() {
     $scope.renderBoard();
   };
@@ -208,53 +441,10 @@ App.controller('Main', function($scope, $http, $location, $timeout, $sce, ngAudi
     $scope.render();
   };
 
-
-  // QUEUE functions
-  //
-  //
-
-  // FETCH functions
-  //
-  //
   /**
-   * Fetch the board
-   * @param  {String} position The URI for the position
-   */
-  $scope.fetchBoard = function (position) {
-    var storageURI = 'https://chess.databox.me/Public/.chess/test';
-    if ($location.search().storageURI) {
-      storageURI = $location.search().storageURI;
-    }
-    $scope.storageURI = storageURI;
-    connectToSocket($scope.storageURI);
-
-
-    f.requestURI(storageURI, undefined, true, function(ok, body) {
-      var p = g.statementsMatching(undefined, URN('fen'));
-      if (p.length) {
-        $scope.position = p[0].object.value;
-        $scope.render();
-      }
-    });
-  };
-
-  /**
-   * Invalidate a cached URI
-   * @param  {String} uri The URI to invalidate
-   */
-  $scope.invalidate = function(uri) {
-    console.log('invalidate : ' + uri);
-    f.unload(uri);
-    f.refresh($rdf.sym(uri));
-  };
-
-  // HELPER functions
-  //
-  //
-  /**
-   * Open a modal dialog
-   * @param  {String} elem  [description]
-   */
+  * openDialog opens a dialog box
+  * @param  {String} elem  The element to display
+  */
   $scope.openDialog = function(elem) {
     LxDialogService.open(elem);
     $(document).keyup(function(e) {
@@ -264,73 +454,6 @@ App.controller('Main', function($scope, $http, $location, $timeout, $sce, ngAudi
     });
   };
 
-  /**
-   * Save the position
-   */
-  $scope.save = function() {
-    var position = $scope.position;
-    if (!position) {
-      LxNotificationService.error('position is empty');
-      return;
-    }
-    console.log(position);
-
-    $http({
-      method: 'PUT',
-      url: $scope.storageURI,
-      withCredentials: true,
-      headers: {
-        "Content-Type": "text/turtle"
-      },
-      data: '<#this> '+ URN('fen') +' """' + position + '""" .',
-    }).
-    success(function(data, status, headers) {
-      LxNotificationService.success('Position saved');
-      $location.search('storageURI', $scope.storageURI);
-      $scope.renderBoard(position);
-    }).
-    error(function(data, status, headers) {
-      LxNotificationService.error('could not save position');
-    });
-
-  };
-
-  /**
-   * TLS Login with WebID
-   */
-  $scope.TLSlogin = function() {
-    $scope.loginTLSButtonText = 'Logging in...';
-    $http({
-      method: 'HEAD',
-      url: AUTHENDPOINT,
-      withCredentials: true
-    }).success(function(data, status, headers) {
-      // add dir to local list
-      var user = headers('User');
-      if (user && user.length > 0 && user.slice(0,4) == 'http') {
-        LxNotificationService.success('Login Successful!');
-        $scope.loggedIn = true;
-        $scope.user = user;
-      } else {
-        LxNotificationService.error('WebID-TLS authentication failed.');
-        console.log('WebID-TLS authentication failed.');
-      }
-      $scope.loginTLSButtonText = 'Login';
-    }).error(function(data, status, headers) {
-      LxNotificationService.error('Could not connect to auth server: HTTP '+status);
-      console.log('Could not connect to auth server: HTTP '+status);
-      $scope.loginTLSButtonText = 'Login';
-    });
-  };
-
-
-  /**
-   * Logout
-   */
-  $scope.logout = function() {
-    $scope.init();
-    LxNotificationService.success('Logout Successful!');
-  };
 
   // SOCKETS
   //
